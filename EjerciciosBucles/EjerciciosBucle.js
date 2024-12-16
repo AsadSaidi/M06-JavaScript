fe03e293c0911fe73c09ce6c8087672aae41fc79
AsadSaidi <asad.saidi.7e8@itb.cat>
2024-12-16 08:55:20 +0100

    16/12/2024

--- EjerciciosBucles/EjerciciosBucle.js
+++ EjerciciosBucles/EjerciciosBucle.js
@@ -1,3 +1,10 @@
-for(let i = 1; i < 11; i++){
-    console.log(i)
+function imprimirNumeros(){
+    let Resultat = 0
+    for(let i = 1; i < 11; i++){
+        console.log(i)
+        Resultat = Resultat + i;
+    }
+    document.getElementById("Resultat").textContent = Resultat
+    document.get
 }
+
